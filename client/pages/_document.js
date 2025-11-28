a1009739ac1245bcc451801c42d6ffa4524478f1
milopalmaerts <milo.palmaerts@gmail.com>
2025-11-28 14:25:40 +0100

    Add Next.js config and document setup

---
+++ client/pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Travel Planner App - Plan your trips and save your favorite places" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
